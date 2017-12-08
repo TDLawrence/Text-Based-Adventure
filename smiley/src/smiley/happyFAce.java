c1c51f4e5cfacd43940efacb62f6f8d6dd40c9df
BT_1N3_28 <BT_1N3_28@BT_1N3_28.bktech.local>
2017-12-08 09:28:01 -0500

    new file

---
+++ smiley/src/smiley/happyFAce.java
@@ -0,0 +1,33 @@
+package smiley;
+
+public class happyFAce {
+
+
+		public static void main(String[] args)
+		{int[][]smileArray=new int[5][5];
+		smileArray[1][1]=9;
+		smileArray[1][3]=9;
+		smileArray[2][2]=4;
+		smileArray[3][0]=1;
+		smileArray[3][4]=1;
+		smileArray[4][1]=1;
+		smileArray[4][2]=1;
+		smileArray[4][3]=1;
+		
+			
+		}
+		public static void print2d(int[][]arr)
+		{for(int[]i:arr)
+		 {for (int j:i)
+		   {if (j!=0)
+		      {System.out.print(j);}
+		    if (j==0)
+		      {System.out.print(" ");} 
+		   }
+			
+		 }
+			System.out.println();
+		}
+	}
+
+
